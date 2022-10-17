c7ba7d7ece2731b6dbee96879f702cb638c1fb2d
0liverScott <97486738+0liverScott@users.noreply.github.com>
2022-10-17 15:24:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'DiWeb.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "P@ssw0rd!!"
+SECRET_KEY = "0000"
